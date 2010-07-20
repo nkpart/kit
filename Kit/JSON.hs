b309820b5e7236a3b0afc214119228ef6666fba9
Nick Partridge <nkpart@gmail.com>
2010-07-21 07:48:36 +1000

    well that didn't work. rolling back some json cleanup

--- Kit/JSON.hs
+++ Kit/JSON.hs
@@ -12,7 +12,8 @@ module Kit.JSON where
           ]
 
       readJSON (JSObject obj) = Kit <$> f "name" <*> f "version"
-        where f = jsonLookup obj
+        where f x = mLookup x jsonObjAssoc >>= readJSON
+              jsonObjAssoc = fromJSObject obj
   
   instance JSON KitSpec where
       showJSON spec = makeObj
@@ -26,10 +27,9 @@ module Kit.JSON where
           let myKit = Kit <$> f "name" <*> f "version"
               myConfig = (KitConfiguration <$> f "dependencies" <*> (f "sourceDir" <|> pure "src"))
            in KitSpec <$> myKit <*> myConfig
-        where f = jsonLookup obj
+        where f x = mLookup x jsonObjAssoc >>= readJSON
+              jsonObjAssoc = fromJSObject obj
   
   mLookup :: Monad m => String -> [(String, b)] -> m b
   mLookup a as = maybe (fail $ "No such element: " ++ a) return (lookup a as)
-  
-  jsonLookup obj key = mLookup key (fromJSObject obj) >>= readJSON
   
