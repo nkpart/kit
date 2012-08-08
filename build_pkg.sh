7bdb34dbc5aa54dc59dd4f7a7727a79427290754
Nick Partridge <nkpart@gmail.com>
2012-08-08 10:15:54 +1000

    0.7.9

--- build_pkg.sh
+++ build_pkg.sh
@@ -2,4 +2,4 @@ cabal build &&
   rm -rf dstroot && 
   mkdir -p dstroot/usr/local/bin && 
   install -m 755 dist/build/kit/kit dstroot/usr/local/bin/ && 
-  /Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker -d etc/kit.pmdoc -o dist/Kit-0.7.7.pkg
+  /Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker -d etc/kit.pmdoc -o dist/Kit-0.7.9.pkg
