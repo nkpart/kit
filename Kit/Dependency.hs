221b47f66358fecd446b76d3db52d2ade31f6cc8
Nick Partridge <nkpart@gmail.com>
2012-10-09 21:44:31 +1000

    .

--- Kit/Dependency.hs
+++ Kit/Dependency.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TupleSections, FlexibleContexts #-}
+{-# LANGUAGE TupleSections #-}
 module Kit.Dependency (
     totalSpecDependencies,
     Dependency,
@@ -30,30 +30,32 @@ instance Packageable Dependency where
 isDevDep :: Dependency -> Bool
 isDevDep = isJust . mbKitPath
 
--- | Return all the (unique) children of this tree (except the top node), in reverse depth order.
-refineDeps :: Eq a => Tree a -> [a]
-refineDeps = nub . concat . reverse . drop 1 . levels
-
 -- todo: check for version ranges :)
 totalSpecDependencies :: KitRepository -> WorkingCopy -> Script [Dependency]
 totalSpecDependencies repo workingCopy = refineDeps <$> dependencyTree repo workingCopy
+
+-- | Return all the (unique) children of this tree (except the top node), in reverse depth order.
+refineDeps :: Eq a => Tree a -> [a]
+refineDeps = nub . concat . reverse . drop 1 . levels
 
 dependencyTree :: KitRepository -> WorkingCopy -> Script (Tree Dependency)
 dependencyTree repo workingCopy = unfoldTreeM (unfoldDeps repo devPackages) baseSpec
   where devPackages = workingDevPackages workingCopy
         baseSpec = workingKitSpec workingCopy
 
+unfoldDeps :: KitRepository -> DevPackages -> KitSpec -> Script (Dependency, [KitSpec])
+unfoldDeps kr devPackages = unfolder (mapM (lookupX kr devPackages) . specDependencies . depSpec) . lookupDependency devPackages 
+
 lookupDependency :: DevPackages -> KitSpec -> Dependency
-lookupDependency devPackages ks = maybe (Dependency ks Nothing) (\(ks',fp) -> Dependency ks' (Just fp)) thisDev
-    where thisDev = findDevPackage devPackages ks
+lookupDependency devPackages ks = maybe (Dependency ks Nothing) (\(devSpec,fp) -> Dependency devSpec (Just fp)) thisDev
+      where thisDev = findDevPackage devPackages ks
+
+unfolder :: Functor m => (a -> m b) -> a -> m (a, b)
+unfolder f b = (b,) <$> f b
+
+lookupX :: MonadIO m => KitRepository -> DevPackages -> Kit -> EitherT String m KitSpec
+lookupX kr devPackages kit = maybe (readKitSpec kr kit) return $ findDevKitSpec devPackages kit 
 
 findDevKitSpec :: DevPackages -> Kit -> Maybe KitSpec
 findDevKitSpec devPackages kit = fmap fst $ findDevPackage devPackages kit
-
-unfoldDeps :: KitRepository -> DevPackages -> KitSpec -> Script (Dependency, [KitSpec])
-unfoldDeps kr devPackages spec = let rootDep = lookupDependency devPackages spec
-                                     lookup' kit = maybe (readKitSpec kr kit) return $ findDevKitSpec devPackages kit 
-                                  in do
-                                      children <- mapM lookup' . specDependencies . depSpec $ rootDep
-                                      return (rootDep, children)
 
